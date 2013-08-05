7f38bdde90aeb36cd1ff227fd4a9f7f282681327
menslow <michael.enslow@gmail.com>
2013-08-05 13:58:31 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skillcult::Application.config.secret_key_base = '076cc7fe64020492611b50010020198b67813a41cb87720382374e92aa5d3ff69d6a70afabdaa045bb4136150f8841d07a865de1c2436e7aa49af847a7dcc8b3'
